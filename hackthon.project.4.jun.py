5959f194371aee7d65be3e21997f82a37049e313
Chitra-karwate <107787254+Chitra-karwate@users.noreply.github.com>
2022-08-11 19:47:29 +0530

    Add files via upload

---
+++ hackthon.project.4.jun.py
@@ -0,0 +1,166 @@
+date=int(input("enter the birthdate"))
+month=int(input("enter the month"))
+if date>31:
+    print("enter your correct birthday")
+if month>12:
+    print("enter the correct month")
+    if month==12:
+      if (date<22):
+        print("sagittarius")
+        astrology=(input("do you want to know about your horoscop"))
+        if astrology==("yes"):
+          print("pisces Health Today You are likely to feel fit and energetic today. Using your high level of energy to your advantage, you may take up some form of recreational activity.")
+        elif astrology==("no"):
+          print("thank you")
+        else:
+         print("ok")
+    else:
+      print("capricorn")
+elif month==1:
+    if (date<20): 
+         print("capricorn")
+         astrology=(input("do you want to know about your horoscop"))
+         if astrology==("yes"):
+             print("You will be comfortable meeting new people. Creative work will be completed. You will complete all your important tasks. Positivity will remain on the edge                      ")
+         elif astrology==("no"):
+            print("thank you")
+         else:
+             ("ok")    
+    else:
+        print("aquarius")
+elif month==2:
+    if (date<19):
+         print("aquarius")
+         astrology=(input("do you want to know about your horoscop "))
+         if astrology=="yes":
+            print("Today your anxiety levels may remain high so make sure you take some time off to get away from issues related to work. You may be feeling a bit insecure, but this is temporary, as all things are, and this too shall pass shortly, and you will be back to being yourself again.")
+         elif astrology=="no":
+            print("thank you")
+         else:
+            print("ok")
+    else:
+        ("pisces")
+elif month==3:
+    if (date<21):
+        print("pisces")
+        astrology=(input("do you want to know about your horoscop"))
+        if astrology=="yes":
+            print("Today you can get everything that you were seeking for. There is no need to be affected by someone who is already trying to play pranks on you. It will be a lucky day for women and they can get everything today. Just make sure to maintain a positive and calm attitude.")
+        elif astrology=="no":
+            print("thank you")
+        else:
+            print("ok")
+    else:
+        print("aries")
+elif month==4:
+    if (date<20):
+        print("aries")
+        astrology=input(" do you want to know about your horoscop")
+        if astrology=="yes":
+            print("Your health is likely to be in excellent condition today. You may be free of underlying diseases. You are likely to be in a good mood, and your positive attitude may manifest itself in your physical condition.")
+        elif astrology=="no":
+            print("thank you")
+        else:
+            print("ok")
+    else:
+        print("taurus")
+elif month==5:
+    if (date<21):
+        print("taurus")
+        astrology=input(" do you want to know about your horoscop")
+        if astrology=="yes":
+            print("Today, you are suggested to keep calm and be happier. Don't overthink about anything and work harder. Don't be too stiff towards people. You have to keep calm and become a little hotter.")
+        elif astrology=="no":
+            print("thank you")
+        else:
+            print("ok")
+    else:
+        ("gemini")
+elif month==6:
+    if (date<21):
+        print("gemini")
+        astrology=input(" do you want to know about your horoscop")
+        if astrology=="yes":
+            print("Today will not be a too stressful day and you are suggested to stay at home and work over your life. Don't be too stressful over anything as it is a perfect day to chill. Involve yourself with the people who make you happy. You can even meet your old buddies and enjoy their company.")
+        elif astrology=="no":
+            print("thank you")
+        else:
+            print("ok")
+    else:
+        print("cancer")
+elif month==7:
+    if (date<23):
+        print("cancer")
+        astrology=input(" do you want to know about your horoscop")
+        if astrology=="yes":
+            print("You might be the man of the house, but to make that house a home, you need a family. So keep that in mind, says Ganesha, and remember that your family is the foundation of your success. Make every possible effort to make your family's roots stronger.")
+        elif astrology=="no":
+            print("thank you")
+        else:
+            print("ok")
+    else:
+        print("leo")
+elif month==8:
+    if(date<23):
+        print("leo")
+        astrology=input("do you want to know about your horoscop")
+        if astrology=="yes":
+            print("Today, as possessive Pluto in your relationship house shoots a harsh beam to passionate Venus, you may have to face some unpleasant truths about a business or romantic partnership. Take a deep breath and initiate a frank discussion about your visions for a shared future.")
+        elif astrology=="no":
+            print("thank you")
+        else:
+            print("ok")
+    else:
+        print("vigro")
+elif month==9:
+    if(date<23):
+        print("vigro")
+        astrology=input(" do you want to know about your horoscop")
+        if astrology=="yes":
+            print("You have a pleasing personality. Today, you will leave quite an impression on others, especially members of the opposite gender. At work, your colleagues will need your expertise to help them through difficult tasks.")
+        elif astrology=="no":
+            print("thank you")
+        else:
+            print("ok")
+    else:
+        print("libra")
+elif month==10:
+    if (date<23):
+        print("libra")
+        astrology=input(" do you want to know about your horoscop")
+        if astrology=="yes":
+            print("Don't focus too much on work and take out some time for yourself from your busy schedule. You can plan lunch or outing with your loved ones and family as spending time with them will help you today. Maintaining a positive attitude throughout the day will help you feel better.")
+        elif astrology=="no":
+            print("thank you")
+        else:
+            print("ok")
+    else:
+        ("scorpio")
+elif month==11:
+    if (date<22):
+       print("scorpio")
+       astrology=input(" do you want to know about your horoscop")
+       if astrology=="yes":
+           print("It will be a smooth day for you and you will get the happiness in your life. No one will able to take your happiness from you. And if you find someone who is doing so, simply stay away from them. There is no need to take too much pressure and simply work hard.")
+       elif astrology=="no":
+             print("thank you")
+       else:
+            print("ok")
+    else:
+       print("sagittarius")
+
+    
+
+
+
+
+
+
+
+    
+    
+
+
+
+
+
